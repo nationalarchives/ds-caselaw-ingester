12369ebf9d9b55e7a7e18b48aba11adda01d144a
James Smith <james@floppy.org.uk>
2022-04-19 16:04:11 +0100

    fix file copy calls

--- ds-caselaw-ingester/lambda_function.py
+++ ds-caselaw-ingester/lambda_function.py
@@ -178,17 +178,17 @@ def handler(event, context):
         docx_filename = extract_docx_filename(metadata)
         if not filename:
             raise DocxFilenameNotFoundException(f'No .docx filename was found in meta. Consignment Ref: {consignment_reference}')
-        copy_file(tar, f'{consignment_reference}/{docx_filename}', f'{uri.replace("/", "_")}.docx', s3_client)
+        copy_file(tar, f'{consignment_reference}/{docx_filename}', f'{uri.replace("/", "_")}.docx', uri, s3_client)
 
         # Store parser log
-        copy_file(tar, f'{consignment_reference}/parser.log', 'parser.log', s3_client)
+        copy_file(tar, f'{consignment_reference}/parser.log', 'parser.log', uri, s3_client)
 
         # Store images
         for image_filename in metadata["parameters"]["TRE"]["payload"]["images"]:
-            copy_file(tar, f'{consignment_reference}/{image_filename}', image_filename, s3_client)
+            copy_file(tar, f'{consignment_reference}/{image_filename}', image_filename, uri, s3_client)
 
         # Copy original tarfile
-        store_file(tar, consignment_reference, filename, s3_client)
+        store_file(open(filename, mode='rb'), uri, os.path.basename(filename), s3_client)
 
         # Notify editors that a new document is ready
         send_new_judgment_notification(uri, metadata)

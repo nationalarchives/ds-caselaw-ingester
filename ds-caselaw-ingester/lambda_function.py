cda05c7a412d3e1c36d38683cdba3568af52aa17
Laura Porter <laura@dxw.com>
2022-05-16 14:57:08 +0100

    Run linter & commit fixes

--- ds-caselaw-ingester/lambda_function.py
+++ ds-caselaw-ingester/lambda_function.py
@@ -1,23 +1,19 @@
-import os
 import json
-
-from typing import Union, Dict, List, Tuple
-
-import boto3
-from boto3.session import Session
-import urllib3
+import os
 import tarfile
 import xml.etree.ElementTree as ET
+from typing import Dict, List, Tuple, Union
 from xml.sax.saxutils import escape
 
-from caselawclient.Client import api_client, MarklogicCommunicationError, MarklogicResourceNotFoundError
+import boto3
+import rollbar
+import urllib3
+from boto3.session import Session
 from botocore.exceptions import NoCredentialsError
+from caselawclient.Client import MarklogicResourceNotFoundError, api_client
 from notifications_python_client.notifications import NotificationsAPIClient
 
-import rollbar
-
-
-rollbar.init(os.getenv('ROLLBAR_TOKEN'), environment=os.getenv('ROLLBAR_ENV'))
+rollbar.init(os.getenv("ROLLBAR_TOKEN"), environment=os.getenv("ROLLBAR_ENV"))
 
 
 class XmlFileNotFoundException(Exception):
@@ -43,7 +39,7 @@ def extract_uri(metadata: dict, consignment_reference: str) -> str:
         uri = uri.replace("https://caselaw.nationalarchives.gov.uk/id/", "")
 
     if not uri:
-        uri = f'failures/{consignment_reference}'
+        uri = f"failures/{consignment_reference}"
 
     return uri
 
@@ -64,84 +60,108 @@ def store_metadata(uri: str, metadata: dict) -> None:
     tdr_metadata = metadata["parameters"]["TDR"]
 
     # Store source information
-    api_client.set_property(uri, name="source-organisation", value=tdr_metadata["Source-Organization"])
+    api_client.set_property(
+        uri, name="source-organisation", value=tdr_metadata["Source-Organization"]
+    )
     api_client.set_property(uri, name="source-name", value=tdr_metadata["Contact-Name"])
-    api_client.set_property(uri, name="source-email", value=tdr_metadata["Contact-Email"])
+    api_client.set_property(
+        uri, name="source-email", value=tdr_metadata["Contact-Email"]
+    )
     # Store TDR data
-    api_client.set_property(uri, name="transfer-consignment-reference", value=tdr_metadata["Internal-Sender-Identifier"])
-    api_client.set_property(uri, name="transfer-received-at",
-                            value=tdr_metadata["Consignment-Completed-Datetime"])
+    api_client.set_property(
+        uri,
+        name="transfer-consignment-reference",
+        value=tdr_metadata["Internal-Sender-Identifier"],
+    )
+    api_client.set_property(
+        uri,
+        name="transfer-received-at",
+        value=tdr_metadata["Consignment-Completed-Datetime"],
+    )
 
 
 def store_file(file, folder, filename, s3_client: Session.client):
-    pathname = f'{folder}/{filename}'
+    pathname = f"{folder}/{filename}"
     try:
-        s3_client.upload_fileobj(file, os.getenv('AWS_BUCKET_NAME'), pathname)
-        print(f'Upload Successful {pathname}')
+        s3_client.upload_fileobj(file, os.getenv("AWS_BUCKET_NAME"), pathname)
+        print(f"Upload Successful {pathname}")
     except FileNotFoundError:
-        print(f'The file {pathname} was not found')
+        print(f"The file {pathname} was not found")
     except NoCredentialsError:
-        print('Credentials not available')
+        print("Credentials not available")
+
 
 def send_new_judgment_notification(uri: str, metadata: dict):
     tdr_metadata = metadata["parameters"]["TDR"]
-    notifications_client = NotificationsAPIClient(os.getenv('NOTIFY_API_KEY'))
+    notifications_client = NotificationsAPIClient(os.getenv("NOTIFY_API_KEY"))
     response = notifications_client.send_email_notification(
-        email_address=os.getenv('NOTIFY_EDITORIAL_ADDRESS'),
-        template_id=os.getenv('NOTIFY_NEW_JUDGMENT_TEMPLATE_ID'),
+        email_address=os.getenv("NOTIFY_EDITORIAL_ADDRESS"),
+        template_id=os.getenv("NOTIFY_NEW_JUDGMENT_TEMPLATE_ID"),
         personalisation={
-            'url': f'{os.getenv("EDITORIAL_UI_BASE_URL")}detail?judgment_uri={uri}',
-            'consignment': tdr_metadata["Internal-Sender-Identifier"],
-            'submitter': f'{tdr_metadata["Contact-Name"]}, {tdr_metadata["Source-Organization"]} <{tdr_metadata["Contact-Email"]}>',
-            'submitted_at': tdr_metadata["Consignment-Completed-Datetime"]
-        }
+            "url": f'{os.getenv("EDITORIAL_UI_BASE_URL")}detail?judgment_uri={uri}',
+            "consignment": tdr_metadata["Internal-Sender-Identifier"],
+            "submitter": f'{tdr_metadata["Contact-Name"]}, {tdr_metadata["Source-Organization"]}'
+            f' <{tdr_metadata["Contact-Email"]}>',
+            "submitted_at": tdr_metadata["Consignment-Completed-Datetime"],
+        },
     )
-    print(f'Sent notification to {os.getenv("NOTIFY_EDITORIAL_ADDRESS")} (Message ID: {response["id"]})')
+    print(
+        f'Sent notification to {os.getenv("NOTIFY_EDITORIAL_ADDRESS")} (Message ID: {response["id"]})'
+    )
+
 
 def send_updated_judgment_notification(uri: str, metadata: dict):
     tdr_metadata = metadata["parameters"]["TDR"]
-    notifications_client = NotificationsAPIClient(os.getenv('NOTIFY_API_KEY'))
+    notifications_client = NotificationsAPIClient(os.getenv("NOTIFY_API_KEY"))
     response = notifications_client.send_email_notification(
-        email_address=os.getenv('NOTIFY_EDITORIAL_ADDRESS'),
-        template_id=os.getenv('NOTIFY_UPDATED_JUDGMENT_TEMPLATE_ID'),
+        email_address=os.getenv("NOTIFY_EDITORIAL_ADDRESS"),
+        template_id=os.getenv("NOTIFY_UPDATED_JUDGMENT_TEMPLATE_ID"),
         personalisation={
-            'url': f'{os.getenv("EDITORIAL_UI_BASE_URL")}detail?judgment_uri={uri}',
-            'consignment': tdr_metadata["Internal-Sender-Identifier"],
-            'submitter': f'{tdr_metadata["Contact-Name"]}, {tdr_metadata["Source-Organization"]} <{tdr_metadata["Contact-Email"]}>',
-            'submitted_at': tdr_metadata["Consignment-Completed-Datetime"]
-        }
+            "url": f'{os.getenv("EDITORIAL_UI_BASE_URL")}detail?judgment_uri={uri}',
+            "consignment": tdr_metadata["Internal-Sender-Identifier"],
+            "submitter": f'{tdr_metadata["Contact-Name"]}, {tdr_metadata["Source-Organization"]} '
+            f'<{tdr_metadata["Contact-Email"]}>',
+            "submitted_at": tdr_metadata["Consignment-Completed-Datetime"],
+        },
     )
-    print(f'Sent notification to {os.getenv("NOTIFY_EDITORIAL_ADDRESS")} (Message ID: {response["id"]})')
+    print(
+        f'Sent notification to {os.getenv("NOTIFY_EDITORIAL_ADDRESS")} (Message ID: {response["id"]})'
+    )
+
 
 def copy_file(tarfile, input_filename, output_filename, uri, s3_client: Session.client):
     file = tarfile.extractfile(input_filename)
     if file:
         store_file(file, uri, output_filename, s3_client)
     else:
-        raise FileNotFoundException(f'File was not found: {input_filename}')
+        raise FileNotFoundException(f"File was not found: {input_filename}")
 
-def send_retry_message(original_message: Dict[str, Union[str, int]], sqs_client: Session.client) -> None:
+
+def send_retry_message(
+    original_message: Dict[str, Union[str, int]], sqs_client: Session.client
+) -> None:
     retry_number = int(original_message["number-of-retries"]) + 1
     if retry_number <= int(os.getenv("MAX_RETRIES", "5")):
         retry_message = {
             "consignment-reference": original_message["consignment-reference"],
             "s3-folder-url": "",
             "consignment-type": original_message["consignment-type"],
-            "number-of-retries": retry_number
+            "number-of-retries": retry_number,
         }
         sqs_client.send_message(
-            QueueUrl=os.getenv('SQS_QUEUE_URL'),
-            MessageBody=json.dumps(retry_message)
+            QueueUrl=os.getenv("SQS_QUEUE_URL"), MessageBody=json.dumps(retry_message)
         )
     else:
-        raise MaximumRetriesExceededException(f'Maximum number of retries reached for {original_message["consignment-reference"]}')
+        raise MaximumRetriesExceededException(
+            f'Maximum number of retries reached for {original_message["consignment-reference"]}'
+        )
 
 
 def create_error_xml_contents(tar, consignment_reference: str):
     try:
-        parser_log = tar.extractfile(f'{consignment_reference}/parser.log')
-        parser_log_contents = escape(parser_log.read().decode('utf-8'))
-        return f'<error>{parser_log_contents}</error>'
+        parser_log = tar.extractfile(f"{consignment_reference}/parser.log")
+        parser_log_contents = escape(parser_log.read().decode("utf-8"))
+        return f"<error>{parser_log_contents}</error>"
     except KeyError:
         return "<error>parser.log not found</error>"
 
@@ -164,41 +184,49 @@ def update_published_documents(uri, s3_client):
 @rollbar.lambda_function
 def handler(event, context):
     decoder = json.decoder.JSONDecoder()
-    message = decoder.decode(event['Records'][0]['Sns']['Message'])
+    message = decoder.decode(event["Records"][0]["Sns"]["Message"])
     consignment_reference = message["consignment-reference"]
 
-    if os.getenv('AWS_ACCESS_KEY_ID') and os.getenv('AWS_SECRET_KEY') and os.getenv('AWS_ENDPOINT_URL'):
-        session = boto3.session.Session(aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
-                                        aws_secret_access_key=os.getenv('AWS_SECRET_KEY'))
-        sqs_client = session.client('sqs', endpoint_url=os.getenv('AWS_ENDPOINT_URL'))
-        s3_client = session.client('s3', endpoint_url=os.getenv('AWS_ENDPOINT_URL'))
+    if (
+        os.getenv("AWS_ACCESS_KEY_ID")
+        and os.getenv("AWS_SECRET_KEY")
+        and os.getenv("AWS_ENDPOINT_URL")
+    ):
+        session = boto3.session.Session(
+            aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
+            aws_secret_access_key=os.getenv("AWS_SECRET_KEY"),
+        )
+        sqs_client = session.client("sqs", endpoint_url=os.getenv("AWS_ENDPOINT_URL"))
+        s3_client = session.client("s3", endpoint_url=os.getenv("AWS_ENDPOINT_URL"))
     else:
         session = boto3.session.Session()
-        sqs_client = session.client('sqs')
-        s3_client = session.client('s3')
+        sqs_client = session.client("sqs")
+        s3_client = session.client("s3")
 
     # Retrieve tar file from S3
     http = urllib3.PoolManager()
     try:
-        file = http.request('GET', message["s3-folder-url"])
+        file = http.request("GET", message["s3-folder-url"])
     except:
         # Send retry message to sqs if the GET fails
         send_retry_message(message, sqs_client)
 
     # Store it in the /tmp directory
-    filename = os.path.join("/tmp", f'{consignment_reference}.tar.gz')
-    with open(filename, 'wb') as out:
+    filename = os.path.join("/tmp", f"{consignment_reference}.tar.gz")
+    with open(filename, "wb") as out:
         out.write(file.data)
         out.close()
 
     # Extract the judgment XML
-    tar = tarfile.open(filename, mode='r')
-    te_metadata_file = tar.extractfile(f'{consignment_reference}/TRE-{consignment_reference}-metadata.json')
-    metadata = decoder.decode(te_metadata_file.read().decode('utf-8'))
+    tar = tarfile.open(filename, mode="r")
+    te_metadata_file = tar.extractfile(
+        f"{consignment_reference}/TRE-{consignment_reference}-metadata.json"
+    )
+    metadata = decoder.decode(te_metadata_file.read().decode("utf-8"))
 
     xml_file_name = metadata["parameters"]["TRE"]["payload"]["xml"]
     try:
-        xml_file = tar.extractfile(f'{consignment_reference}/{xml_file_name}')
+        xml_file = tar.extractfile(f"{consignment_reference}/{xml_file_name}")
     except KeyError:
         xml_file = None
 
@@ -206,10 +234,12 @@ def handler(event, context):
 
     if xml_file:
         contents = xml_file.read()
-    elif 'failures' in uri:
+    elif "failures" in uri:
         contents = create_error_xml_contents(tar, consignment_reference)
     else:
-        raise XmlFileNotFoundException(f'No XML file was found. Consignment Ref: {consignment_reference}')
+        raise XmlFileNotFoundException(
+            f"No XML file was found. Consignment Ref: {consignment_reference}"
+        )
 
     ET.register_namespace("", "http://docs.oasis-open.org/legaldocml/ns/akn/3.0")
     ET.register_namespace("uk", "https://caselaw.nationalarchives.gov.uk/akn")
@@ -221,12 +251,12 @@ def handler(event, context):
 
         # Notify editors that a document has been updated
         send_updated_judgment_notification(uri, metadata)
-        print(f'Updated judgment {uri}')
+        print(f"Updated judgment {uri}")
     except MarklogicResourceNotFoundError:
         api_client.insert_judgment_xml(uri, xml)
         # Notify editors that a new document is ready
         send_new_judgment_notification(uri, metadata)
-        print(f'Inserted judgment {uri}')
+        print(f"Inserted judgment {uri}")
 
     # Store metadata
     store_metadata(uri, metadata)
@@ -234,21 +264,37 @@ def handler(event, context):
     # Store docx and rename
     docx_filename = extract_docx_filename(metadata)
     if not filename:
-        raise DocxFilenameNotFoundException(f'No .docx filename was found in meta. Consignment Ref: {consignment_reference}')
-    copy_file(tar, f'{consignment_reference}/{docx_filename}', f'{uri.replace("/", "_")}.docx', uri, s3_client)
+        raise DocxFilenameNotFoundException(
+            f"No .docx filename was found in meta. Consignment Ref: {consignment_reference}"
+        )
+    copy_file(
+        tar,
+        f"{consignment_reference}/{docx_filename}",
+        f'{uri.replace("/", "_")}.docx',
+        uri,
+        s3_client,
+    )
 
     # Store parser log
     try:
-        copy_file(tar, f'{consignment_reference}/parser.log', 'parser.log', uri, s3_client)
+        copy_file(
+            tar, f"{consignment_reference}/parser.log", "parser.log", uri, s3_client
+        )
     except KeyError:
         pass
 
     # Store images
     for image_filename in metadata["parameters"]["TRE"]["payload"]["images"]:
-        copy_file(tar, f'{consignment_reference}/{image_filename}', image_filename, uri, s3_client)
+        copy_file(
+            tar,
+            f"{consignment_reference}/{image_filename}",
+            image_filename,
+            uri,
+            s3_client,
+        )
 
     # Copy original tarfile
-    store_file(open(filename, mode='rb'), uri, os.path.basename(filename), s3_client)
+    store_file(open(filename, mode="rb"), uri, os.path.basename(filename), s3_client)
 
     if api_client.get_published(uri):
         update_published_documents(uri, s3_client)

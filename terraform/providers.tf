2e7d6af3f31cb462cb684fbe3d03ef8fb718b0c8
anthonyhashemi <ant.hashemi@gmail.com>
2026-04-17 16:33:08 +0100

    infra: use partial S3 backend config in providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -8,13 +8,13 @@ terraform {
     }
   }
 
-  # Backend configuration should be provided per-environment.
-  # Example for S3 backend:
-  # backend "s3" {
-  #   bucket = "my-terraform-state"
-  #   key    = "ds-caselaw-ingester/terraform.tfstate"
-  #   region = "eu-west-2"
-  # }
+  # Backend bucket is provided at init time via:
+  #   terraform init -backend-config="bucket=<bucket-name>"
+  # CI passes this from the TF_BACKEND_BUCKET secret.
+  backend "s3" {
+    key    = "ds-caselaw-ingester/terraform.tfstate"
+    region = "eu-west-2"
+  }
 }
 
 provider "aws" {

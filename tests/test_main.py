18c53993229ba3b19540f21ff514effffe5dc469
Nick Jackson <nick@dxw.com>
2026-05-13 17:31:19 +0100

    Move metadata storage tests to own file and extend with negative cases
    These now test that the correct storage methods are called based on the provided metadata.

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,6 @@
 import copy
 import json
-from unittest.mock import ANY, MagicMock, PropertyMock, call, patch
+from unittest.mock import ANY, MagicMock, PropertyMock, patch
 
 import lxml.etree as ET
 import pytest
@@ -20,47 +20,8 @@ from .conftest import s3_message_raw, v2_message
 
 rollbar.init(access_token=None, enabled=False)
 
-NULL_UPDATE_METADATA = '{\n  "Judgment-Update": null,\n  "Judgment-Update-Type": null,\n  "Judgment-Update-Details": null,\n  "Judgment-Neutral-Citation": null,\n  "Judgment-No-Neutral-Citation": null,\n  "Judgment-Reference": null\n}'
-
 
 class TestLambda:
-    def test_store_tdr_metadata(self, v2_ingest):
-        v2_ingest.uri = "uri"
-        v2_ingest.api_client.set_property = MagicMock()
-
-        v2_ingest.store_tdr_metadata(
-            {
-                "Source-Organization": "Ministry of Justice",
-                "Contact-Name": "Tom King",
-                "Internal-Sender-Identifier": "TDR-2021-CF6L",
-                "Consignment-Completed-Datetime": "2021-12-16T14:54:06Z",
-                "Contact-Email": "someone@example.com",
-                "Judgment-Neutral-Citation": "[2019] UKSC 1701",
-            },
-        )
-
-        v2_ingest.api_client.set_property.assert_has_calls(
-            [
-                call("uri", name="source-organisation", value="Ministry of Justice"),
-                call("uri", name="source-name", value="Tom King"),
-                call("uri", name="source-email", value="someone@example.com"),
-                call("uri", name="transfer-consignment-reference", value="TDR-2021-CF6L"),
-                call("uri", name="transfer-received-at", value="2021-12-16T14:54:06Z"),
-            ],
-        )
-
-    def test_store_parser_metadata(self, v2_ingest):
-        v2_ingest.uri = "uri"
-        v2_ingest.api_client.set_property = MagicMock()
-
-        v2_ingest.store_parser_metadata({"parser_run_id": "607e7ef1-3b5e-431b-b115-bb1811767f5c"})
-
-        v2_ingest.api_client.set_property.assert_has_calls(
-            [
-                call("uri", name="parser-run-id", value="607e7ef1-3b5e-431b-b115-bb1811767f5c"),
-            ],
-        )
-
     def test_get_consignment_reference_success_v2(self):
         message = copy.deepcopy(v2_message)
         message["parameters"]["reference"] = "THIS_REF"
